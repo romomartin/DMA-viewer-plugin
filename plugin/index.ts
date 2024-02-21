8668dfb76cfcb7f3252a2161eb52c8cdde87885a
Alex Torres <alex.torres@qatium.com>
2024-02-21 11:43:46 +0100

    Init commit

---
+++ plugin/index.ts
@@ -0,0 +1,44 @@
+import { AssetStatus, OverlayLayer, PluginI, SDK, ValveFamilies } from "@qatium/plugin";
+
+class Plugin implements PluginI {
+  onNetworkChanged() {
+    console.log("network changed");
+  }
+
+  run(sdk: SDK) {
+    const hydrants = sdk.network.getJunctions((j) => j.group === 'hydrant');
+
+    sdk.map.addOverlay([
+      {
+        type: 'HeatmapLayer',
+        id: 'density',
+        data: hydrants.map((h) => ({ geometry: h.geometry, properties: {}, type: 'Feature' })),
+        opacity: 0.5,
+        visible: true,
+        getPosition: (j) => j.geometry.coordinates,
+        radiusPixels: 25
+      } as OverlayLayer<"HeatmapLayer">
+    ]);
+
+    sdk.ui.sendMessage(sdk.network.getValves((a) => (
+      a.family === ValveFamilies.TCV &&
+      !!a.simulation &&
+      a.simulation.status === AssetStatus.CLOSED
+    )).length);
+  }
+
+  onMessage(sdk: SDK, message: unknown) {
+    if (typeof message !== "string") {
+      return;
+    }
+
+    switch(message) {
+      case "zoomMap":
+        return sdk.map.fitTo(["Mandalay_P1"], { flightDuration: 2000 });
+      default:
+        return;
+    }
+  }
+}
+
+register(new Plugin());
