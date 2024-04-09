2921494325e4618941cc9e5094416e7c4c064532
romomartin <ander.romo@qatium.com>
2024-04-09 23:06:29 +0200

    Add dmas and color picker to panel

--- ui/panel/Panel.tsx
+++ ui/panel/Panel.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import "./panel.css";
 import { DMA, Message, events } from "../../plugin/types";
 import { onMessage } from "@qatium/plugin/ui";
+import { ColorResult } from "react-color";
+import { COLORS } from "../../plugin/colors"
+import Circle from "react-color/lib/components/circle/Circle";
 
 export const Panel = () => { 
   const [dmas, setDmas] = useState<DMA[]>([]);
@@ -13,8 +17,54 @@ export const Panel = () => {
   
   return (
     <div className="panel">
-      some content {dmas.length}
-      {/* <PanelBody dmas={dmas}></PanelBody> */}
+      <PanelBody dmas={dmas}></PanelBody>
     </div>
+  );
+};
+
+const PanelBody = ({ dmas }: { dmas: DMA[] }) => {
+  return (
+    <div className="panel-body">
+      {dmas?.map((dma, i) => {
+        return <Row key={i} dma={dma}></Row>;
+      })}
+    </div>
+  );
+};
+
+const Row = ({ dma }: { dma: DMA }) => {
+  const [showColorPicker, setShowColorPicker] = useState<boolean>(false);
+  const [dmaColor, setDmaColor] = useState<string>(dma.color);
+
+  const displayColorPicker = () => {
+    setShowColorPicker(!showColorPicker);
+  };
+
+  const handleColorChange = (color: ColorResult) => {
+    setDmaColor(color.hex);
+    displayColorPicker();
+  };
+
+  return (
+    <>
+      <div className="row">
+        <button
+          className={showColorPicker ? "selected" : ""}
+          aria-label={`${dma.id}-color-picker-button`}
+          onClick={displayColorPicker}
+          style={{ backgroundColor: dmaColor }}
+        ></button>
+        <div className="text">{dma.id}</div>
+      </div>
+      {showColorPicker && (
+        <Circle
+          colors={COLORS}
+          width={"100%"}
+          circleSize={17}
+          circleSpacing={8}
+          onChangeComplete={handleColorChange}
+        ></Circle>
+      )}
+    </>
   );
 };
