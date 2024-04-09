68b612742a864f4c4b50ae0a410ee8483593ee40
romomartin <ander.romo@qatium.com>
2024-04-09 23:25:36 +0200

    Add overlay with colored pipes for DMAs

---
+++ plugin/build-pipe-layer.ts
@@ -0,0 +1,67 @@
+import { OverlayLayer, Pipe } from "@qatium/plugin/engine";
+import { Color } from "@deck.gl/core/typed";
+import { Feature } from "geojson";
+
+export const buildPipeLayer = (
+  id: string,
+  pipes: Pipe[],
+  color: string
+): OverlayLayer<"GeoJsonLayer"> => {
+  return {
+    type: "GeoJsonLayer",
+    id,
+    data: pipes.map(
+      (pipe) =>
+        ({
+          type: "Feature",
+          geometry: pipe.geometry,
+          properties: {},
+        } as Feature)
+    ),
+    opacity: 1,
+    getLineColor: hexToRgb(color),
+    getLineWidth: 2,
+    lineWidthUnits: "pixels",
+    stroked: true,
+    lineJointRounded: true,
+    lineCapRounded: true,
+    visible: true,
+  };
+};
+  
+// const hexToRgb = (hex: string): Color => {
+//   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+//   return result
+//     ? [
+//         parseInt(result[1], 16),
+//         parseInt(result[2], 16),
+//         parseInt(result[3], 16),
+//       ]
+//     : [0, 0, 0];
+// };
+
+const hexToRgb = (hex: string): Color => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? [
+        hexToDecimal(result[1]),
+        hexToDecimal(result[2]),
+        hexToDecimal(result[3]),
+      ]
+    : [0, 0, 0];
+};
+
+const hexToDecimal = (hexString: string): number => {
+  const hexDigits = "0123456789ABCDEF";
+  let decimalNumber = 0;
+
+  for (let i = 0; i < hexString.length; i++) {
+    const digit = hexDigits.indexOf(hexString[i].toUpperCase());
+    if (digit === -1) {
+      throw new Error("Invalid hexadecimal string");
+    }
+    decimalNumber = decimalNumber * 16 + digit;
+  }
+
+  return decimalNumber;
+};
--- plugin/index.ts
+++ plugin/index.ts
@@ -1,9 +1,10 @@
-import { PluginI, SDK, registerPlugin } from "@qatium/plugin/engine";
+import { OverlayLayer, PipeGroups, PluginI, SDK, registerPlugin } from "@qatium/plugin/engine";
 import { Message, events } from "./types";
 import { getColor } from "./colors";
+import { buildPipeLayer } from "./build-pipe-layer";
 
 class Plugin implements PluginI<Message> {
-  selectedElement: ReturnType<SDK["map"]["getSelectedElement"]>;
+  overlay: OverlayLayer<"GeoJsonLayer">[] = [];
 
   run(sdk: SDK) {
     const dmas = sdk.network
@@ -11,6 +12,17 @@ class Plugin implements PluginI<Message> {
         .map((zone, i) => ({ id: zone.id, color: getColor(i) }));
   
       sdk.ui.sendMessage<Message>({ event: events.getDMAs, dmas });
+
+      dmas.forEach((dma) => {
+        const dmaPipes = sdk.network
+          .getZone(dma.id)
+          ?.getPipes((pipe) => pipe.group === PipeGroups.Main);
+  
+        if (dmaPipes && dmaPipes.length > 0)
+          this.overlay.push(buildPipeLayer(dma.id, dmaPipes, dma.color));
+      });
+
+      sdk.map.addOverlay(this.overlay);
   }
 }
 
