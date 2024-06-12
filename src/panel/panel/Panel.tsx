085fdb359672b285ee3ce1f0f18cb1e9db11bb01
romomartin <ander.romo@qatium.com>
2024-06-12 20:46:45 +0200

    Update unrecognized DMAs handling style

--- src/panel/panel/Panel.tsx
+++ src/panel/panel/Panel.tsx
@@ -20,45 +20,53 @@ export const Panel = () => {
 };
 
 const PanelBody = ({ dmas }: { dmas: DMA[] }) => {
-  if (dmas.length === 0)
-    return (
-      <div className="panel-body">
-        <p>
-          No DMAs in your network yet.{" "}
-          <a
-            href="https://help.qatium.com/hc/en-us/articles/23182710465937-Zones"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Add a zone layer to start
-          </a>
-        </p>
-      </div>
-    );
+  if (dmas.length === 0) return <NoDmasYet></NoDmasYet>;
+
+  const { dmasWithPipes, dmasWithoutPipes } = dmas.reduce(
+    (acc, dma) => {
+      dma.hasPipes
+        ? acc.dmasWithPipes.push(dma)
+        : acc.dmasWithoutPipes.push(dma);
+      return acc;
+    },
+    {
+      dmasWithPipes: [],
+      dmasWithoutPipes: []
+    } as { dmasWithPipes: DMA[]; dmasWithoutPipes: DMA[] }
+  );
 
   return (
     <div className="panel-body">
-      {dmas.map((dma, i) => (
-        <Row key={i} dma={dma}></Row>
+      {dmasWithoutPipes.length > 0 && dmasWithPipes.length > 0 && <h4>DMAs</h4>}
+      {dmasWithPipes.map((dma, i) => (
+        <DmaRow key={i} dma={dma}></DmaRow>
       ))}
-      {dmas.some((dma) => !dma.hasPipes) && (
-        <p className="bottom-warning">
-          Some DMAs don&apos;t have any pipes assigned. Define the zone inlets
-          first.{" "}
-          <a
-            href="https://help.qatium.com/hc/en-us/articles/23182710465937-Zones"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Learn how
-          </a>
-        </p>
+      {dmasWithoutPipes.length > 0 && (
+        <UnrecognizedDmas dmas={dmasWithoutPipes}></UnrecognizedDmas>
       )}
     </div>
   );
 };
 
-const Row = ({ dma }: { dma: DMA }) => {
+const NoDmasYet = () => {
+  return (
+    <div className="panel-body">
+      <p className="warning-text">
+        No DMAs in your network yet.{" "}
+        <a
+          href="https://help.qatium.com/hc/en-us/articles/23182710465937-Zones"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Add a zone layer to start
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
+
+const DmaRow = ({ dma }: { dma: DMA }) => {
   const [showColorPicker, setShowColorPicker] = useState<boolean>(false);
   const [pickerColor, setPickerColor] = useState<string>(dma.color);
 
@@ -81,19 +89,12 @@ const Row = ({ dma }: { dma: DMA }) => {
   return (
     <>
       <div className="row">
-        {dma.hasPipes && (
-          <button
-            className={showColorPicker ? "selected" : ""}
-            aria-label={`${dma.id}-color-picker-button`}
-            onClick={toggleColorPicker}
-            style={{ backgroundColor: pickerColor }}
-          ></button>
-        )}
-        {!dma.hasPipes && (
-          <div className="warning">
-            <IconWarning></IconWarning>
-          </div>
-        )}
+        <button
+          className={showColorPicker ? "selected" : ""}
+          aria-label={`${dma.id}-color-picker-button`}
+          onClick={toggleColorPicker}
+          style={{ backgroundColor: pickerColor }}
+        ></button>
         <div className="text">{dma.id}</div>
       </div>
       {showColorPicker && (
@@ -105,6 +106,38 @@ const Row = ({ dma }: { dma: DMA }) => {
           onChangeComplete={handleColorChange}
         ></Circle>
       )}
+    </>
+  );
+};
+
+const UnrecognizedDmas = ({ dmas }: { dmas: DMA[] }) => {
+  return (
+    <>
+      <h4>Unrecognized DMAs</h4>
+      <div className="row-warning">
+        <div className="icon">
+          <IconWarning></IconWarning>
+        </div>
+        <div>
+          <p className="warning-text">
+            Some DMAs don&apos;t have any pipes assigned.<br></br>Define the
+            zone inlets first.{" "}
+            <a
+              href="https://help.qatium.com/hc/en-us/articles/23182710465937-Zones"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Learn how
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+      <ul className="unrecognized-list">
+        {dmas.map((dma, i) => (
+          <li key={i}>{dma.id}</li>
+        ))}
+      </ul>
     </>
   );
 };
